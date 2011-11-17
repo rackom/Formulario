6396b4f192c654e6004ed27c750f9404f4692f3b
rackom <rackom10@gmail.com>
2011-11-17 10:24:26 +0100

    Initial Commit

---
+++ Formulario 2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Formulario 2
+//
+//  Created by Michal Racko on 11/17/11.
+//  Copyright (c) 2011 STU - FIIT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate, UITabBarControllerDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) UITabBarController *tabBarController;
+
+@end
---
+++ Formulario 2/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  Formulario 2
+//
+//  Created by Michal Racko on 11/17/11.
+//  Copyright (c) 2011 STU - FIIT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
